6d899ddcac54e7e2d8e75ce2acf1c7a559a2d962
Christoph Gehl <chtistoph.gehl@gmail.com>
2016-03-01 10:21:40 +0100

    GLSL - vertex and fragment shader added

--- FishingSimulator/src/core/Game.java
+++ FishingSimulator/src/core/Game.java
@@ -3,23 +3,32 @@ package core;
 import org.lwjgl.util.vector.Vector3f;
 
 import render.Mesh;
+import render.Shader;
 import render.Vertex;
 
 public class Game {
 
 	private Mesh mesh;
-	
-	
+	private Shader shader;
+
 	public Game() {
 		mesh = new Mesh();
+		shader = new Shader();
 
 		Vertex[] data = new Vertex[] { 
 				new Vertex(new Vector3f(-1, -1, 0)),
-				new Vertex(new Vector3f(-1, 1, 0)), 
 				new Vertex(new Vector3f(0, 1, 0)),
-			};
-		
+				new Vertex(new Vector3f(1, -1, 0)),
+				
+		};
+
 		mesh.addVertices(data);
+
+		shader.addVertexShader(RessourceLoader.loadShader("vertex.vs"));
+		shader.addFragmentShader(RessourceLoader.loadShader("fragment.vs"));
+		
+		shader.compileShader();
+		shader.bind();
 
 	}
 
